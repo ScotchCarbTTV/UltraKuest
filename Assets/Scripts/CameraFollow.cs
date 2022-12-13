1200f2b2a7f7cc62c9b7b687b7da562e30635b72
ScotchCarbTTV <70674628+ScotchCarbTTV@users.noreply.github.com>
2022-12-13 18:59:15 +0800

    Multiple changes woooo

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //variable for camera
+    //[SerializeField] private GameObject cameraContainer;
+
+    //variable for the player object
+    [SerializeField] private GameObject player;
+
+    //offset to maintain from player
+    [SerializeField] float offSet;
+
+    //variable for camera height
+    [SerializeField] float camHeight;
+
+    //float for the camera follow speed
+    [SerializeField] float followSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+
+        if(Vector3.Distance(transform.position, player.transform.position) > offSet)
+        {
+            transform.position = Vector3.Slerp(transform.position, new Vector3(player.transform.position.x, camHeight, player.transform.position.z), followSpeed * Time.deltaTime); 
+        }
+
+        //transform.position = Vector3.Slerp(transform.position, player.transform.forward + offSet, followSpeed * Time.deltaTime) ;
+    }
+}
